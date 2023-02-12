f15b4601553daea5062a708f14dfe2bb73defaf7
Pelayodc <uo281956@uniovi.es>
2023-02-12 12:38:08 +0100

    SDI-2223-703-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Sdi2223703SpringApplication.class);
+    }
+
+}
