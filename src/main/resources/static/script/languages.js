1d401019e7875c2f6a74600dda4f48c8c76859ca
Pelayodc <uo281956@uniovi.es>
2023-02-21 18:41:54 +0100

    SDI-2223-703-3.2-Internacionalización.

---
+++ src/main/resources/static/script/languages.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#languageDropDownMenuButton a").click(function (e) {
+        e.preventDefault();
+        let languageSelectedText = $(this).text();
+        let languageSelectedValue = $(this).attr("value");
+
+        $("#btnLanguage").text(languageSelectedText);
+        window.location.replace('?lang=languageSelectedValue');
+        return false;
+    });
+});
