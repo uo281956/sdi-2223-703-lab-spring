a1143c84d2da3b1ea2cf60d22f93f45c9bc0d8a8
Pelayodc <uo281956@uniovi.es>
2023-03-01 19:04:11 +0100

    SDI-2223-703-4.4-SpringBoot-Consulta.

--- src/main/java/com/uniovi/sdi/repositories/MarksRepository.java
+++ src/main/java/com/uniovi/sdi/repositories/MarksRepository.java
@@ -1,10 +1,13 @@
 package com.uniovi.sdi.repositories;
 
 import com.uniovi.sdi.entities.Mark;
+import com.uniovi.sdi.entities.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface MarksRepository extends CrudRepository<Mark, Long> {
 
@@ -12,4 +15,7 @@ public interface MarksRepository extends CrudRepository<Mark, Long> {
     @Transactional
     @Query("UPDATE Mark SET resend = ?1 WHERE id = ?2")
     void updateResend(Boolean resend, Long id);
+
+    @Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC")
+    List<Mark> findAllByUser(User user);
 }
