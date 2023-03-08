7473e8f1e1249113b352cfe5dc289dbeb766b663
Pelayodc <uo281956@uniovi.es>
2023-03-08 19:01:14 +0100

    SDI-2223-703-5.1-Clase principal.

--- src/test/java/com/uniovi/sdi/Sdi2223703SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi/Sdi2223703SpringApplicationTests.java
@@ -1,10 +1,48 @@
 package com.uniovi.sdi;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class Sdi2223703SpringApplicationTests {
+
+    //static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+    //static String Geckodriver = "C:\\Path\\geckodriver-v0.30.0-win64.exe";
+    //static String Geckodriver = "C:\\Dev\\tools\\selenium\\geckodriver-v0.30.0-win64.exe";
+    static String PathFirefox = "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
+    static String Geckodriver = "/Users/pelayodc/Downloads/PL-SDI-Sesión5-material/PL-SDI-Sesión5-material/geckodriver-v0.30.0-macos";
+
+    //Común a Windows y a MACOSX
+    static WebDriver driver = getDriver(PathFirefox, Geckodriver);
+    static String URL = "http://localhost:8090";
+    public static WebDriver getDriver(String PathFirefox, String Geckodriver) {
+        System.setProperty("webdriver.firefox.bin", PathFirefox);
+        System.setProperty("webdriver.gecko.driver", Geckodriver);
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    @BeforeEach
+    public void setUp(){
+        driver.navigate().to(URL);
+    }
+    //Después de cada prueba se borran las cookies del navegador
+    @AfterEach
+    public void tearDown(){
+        driver.manage().deleteAllCookies();
+    }
+    //Antes de la primera prueba
+    @BeforeAll
+    static public void begin() {}
+    //Al finalizar la última prueba
+    @AfterAll
+    static public void end() {
+    //Cerramos el navegador al finalizar las pruebas
+        driver.quit();
+    }
 
     @Test
     void contextLoads() {
